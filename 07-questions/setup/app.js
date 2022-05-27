e8a116b24b3a5fad6431a59c0d9a6016c4acff11
Nicholas Oliveira <nicholasoliveira@Nicholass-MacBook-Pro.local>
2022-05-26 20:28:24 -0700

    Finished: traversing the dom, using selectors

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,10 +1,21 @@
 //using selectors inside the element
-// traversing the dom
 
-const btns = document.querySelectorAll(".question-btn");
+const questions = document.querySelectorAll(".question");
 
-btns.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    console.log(e.currentTarget);
+questions.forEach((question) => {
+  const btn = question.querySelector(".question-btn");
+  btn.addEventListener("click", () => {
+    question.classList.toggle("show-text");
   });
 });
+
+// traversing the dom
+
+// const btns = document.querySelectorAll(".question-btn");
+
+// btns.forEach((btn) => {
+//   btn.addEventListener("click", (e) => {
+//     const question = e.currentTarget.parentElement.parentElement;
+//     question.classList.toggle("show-text");
+//   });
+// });
