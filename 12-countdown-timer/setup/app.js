408b3831144cdc701f7c19c2085c924ec9554270
Nick Oliveira <87996659+noliveira95@users.noreply.github.com>
2022-06-07 19:57:51 -0700

    Set up date to display dynamically, starting timer

--- 12-countdown-timer/setup/app.js
+++ 12-countdown-timer/setup/app.js
@@ -21,3 +21,19 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector(".giveaway");
+const deadline = document.querySelector(".deadline");
+const timeElements = document.querySelectorAll(".deadline h4");
+
+// Future date is July 1, 2022 at 6:00 am
+let futureDate = new Date(2022, 6, 1, 6, 0);
+
+const year = futureDate.getFullYear();
+const month = months[futureDate.getMonth()];
+const day = weekdays[futureDate.getDay()];
+const date = futureDate.getDate();
+const hours = futureDate.getHours();
+const mins = futureDate.getMinutes();
+
+giveaway.textContent = `Giveaway ends ${day}, ${month} ${date}, ${year} at ${hours}:${mins}`;
